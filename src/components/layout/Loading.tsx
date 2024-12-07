b08a51af430d4771df594d5124ab4c168922728e
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2024-12-07 00:35:10 -0300

    Alterando tamanho do Spinner

--- src/components/layout/Loading.tsx
+++ src/components/layout/Loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "flowbite-react";
 const Loading = () => {
   return (
     <div className="min-h-[108vh] w-full flex items-center justify-center bg-[#0000001f] absolute z-20 top-0 backdrop-blur-[0.5px]">
-      <Spinner className="size-[30px] text-[#ffffffda] fill-[#4F47A8]" />
+      <Spinner className="size-[25px] text-[#ffffffda] fill-[#4F47A8]" />
     </div>
   );
 };
