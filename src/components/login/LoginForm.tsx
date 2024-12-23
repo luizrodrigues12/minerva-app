d6dff07a8be0038ea001128cbc14695f9d445f59
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2024-12-23 19:45:33 -0300

    Corrigindo dados do usuário usando window ao invés de router

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -26,7 +26,7 @@ const LoginForm = () => {
       const { error } = await res.json();
       if (error) throw new Error(error);
       //Enviando para a home.
-      router.push("/home");
+      window.location.assign("/home");
     } catch (err: any) {
       setError(err.message);
     }
