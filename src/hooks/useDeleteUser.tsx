fe57f0df77516d09e8a1f3a79f682511090eff5c
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2024-12-23 19:34:07 -0300

    Corrigindo delete user

--- src/hooks/useDeleteUser.tsx
+++ src/hooks/useDeleteUser.tsx
@@ -1,2 +1,17 @@
+import { getCookie } from "cookies-next";
+import { useMutation } from "@tanstack/react-query";
 
+const token = getCookie("authorization");
+
+export function useDeleteUser() {
+  const deleteUser = async () => {
+    await fetch(`${process.env.HOST}/api/user/delete_user`, {
+      method: "POST",
+      body: JSON.stringify({ token: token }),
+    });
+  };
+
+  const mutate = useMutation({ mutationFn: deleteUser });
+
+  return mutate;
 }
