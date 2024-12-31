71a7988ee83cbb58888e82797612c75e9e0d028f
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2024-12-31 11:44:46 -0300

    Adicionando tipagem ao hook useAddStudent

--- src/hooks/useAddStudent.tsx
+++ src/hooks/useAddStudent.tsx
@@ -1,3 +1,6 @@
+import { idToSubjects } from "@/actions/idToSubjects";
+import { MateriaType } from "@/models/MateriasModel";
+import { AlunoObj } from "@/models/userModel";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { getCookie } from "cookies-next";
 
@@ -34,6 +37,36 @@ export function useAddStudent(
 
   const mutate = useMutation({
     mutationFn: postStudent,
+
+    // onMutate: async (variables) => {
+    //   await queryClient.cancelQueries({ queryKey: ["alunos-data"] });
+
+    //   const oldStudents = queryClient.getQueryData(["alunos-data"]);
+
+    //   queryClient.setQueryData(
+    //     ["alunos-data"],
+    //     async (oldStudents: Array<AlunoObj>) => {
+    //       const subjectsNew: Array<MateriaType> = await idToSubjects(
+    //         JSON.stringify(variables.checkedsSubjects])
+    //       );
+    //       return [
+    //         ...oldStudents,
+    //         {
+    //           idAluno: variables.idStudent,
+    //           nome: variables.nomeAluno,
+    //           preparatorio: variables.checkedsPrep,
+    //           materias: subjectsNew,
+    //         },
+    //       ];
+    //     }
+    //   );
+
+    //   return { oldStudents };
+    // },
+
+    // onError(error, variables, context) {
+    //   queryClient.setQueryData(["alunos-data"], context?.oldStudents);
+    // },
 
     onSuccess(data, variables, context) {
       queryClient.invalidateQueries({ queryKey: ["alunos-data"] });
