d4d7805609d048fa0570ee76afff2d8eff46bad2
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2025-01-05 22:05:39 -0300

    style: reestilizando resetPass e forgetPass.

--- src/components/forget_pass/ForgetPassForm.tsx
+++ src/components/forget_pass/ForgetPassForm.tsx
@@ -3,18 +3,17 @@
 import { validateEmail } from "@/utils/regex";
 import { useRouter } from "nextjs-toploader/app";
 import { useState } from "react";
+import { motion } from "motion/react";
 
 const ForgetPassForm = () => {
   const router = useRouter();
-  const [error, setError] = useState();
   const [email, setEmail] = useState("");
-
+  const [emailError, setEmailError] = useState("");
   const emailTest = validateEmail.test(email);
 
-  const handleForm = async (e: any) => {
+  const handleForgetPass = async () => {
     try {
       if (!emailTest) throw new Error("Por favor, digite um email válido.");
-      e.preventDefault();
       //Enviando post dos dados
       const response = await fetch(
         `${process.env.HOST}/api/user/forget_password/`,
@@ -25,49 +24,57 @@ const ForgetPassForm = () => {
       );
       router.push("/login");
     } catch (error: any) {
-      setError(error.message);
+      setEmailError(error.message);
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-full height_pattern">
-      <div className="px-8 md:self-center rounded-lg md:px-6 md:py-5 md:w-[400px] md:border-zinc-800 md:border-2">
-        <form
-          method="POST"
-          onSubmit={handleForm}
-          className="form_login_register"
-        >
-          <div className="flex flex-col gap-2">
-            <h1 className="h1_form">Redefinir Senha</h1>
+    <section className="flex flex-col min-h-[76vh] font-inter text-black xl:min-h-[69.4vh] bg-background01 items-center">
+      <div className="bg-background02 p-8 rounded-md shadow-md mx-8 my-[32px] md:w-[400px]">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-[20px] md:text-[20px] lg:text-[22px] xl:text-[22px] text-[#353535]">
+            Redefinir Senha
+          </h1>
 
-            <p className="text-sm lg:text-[16px] leading-5 w-[100%]">
-              Caso exista uma conta com esse email, você receberá um link para
-              alterar sua senha.
-            </p>
+          <p className="text-[14px] md:text-[15px] bg-background01 p-3 rounded-md">
+            Caso exista uma conta com esse email, você receberá um email com um
+            link para redefinir sua senha.
+          </p>
 
-            <div className="div_form">
-              <input
-                type="email"
-                name="email"
-                id=""
-                placeholder="Email"
-                className="input_email_username"
-                value={email}
-                onChange={(e) => {
-                  e.preventDefault();
-                  setEmail(e.target.value);
-                }}
-                required
-              />
-            </div>
-            {error ? <p>{error}</p> : <></>}
-            <button type="submit" className="btn_submit_form">
-              Enviar Email
-            </button>
+          <div>
+            <div className="text-[16px]">email</div>
+            <input
+              type="email"
+              name="email"
+              autoComplete="email"
+              className="flex items-center p-2.5 border-0 bg-background01 rounded-[7px] text-[14px] w-full mt-2 md:text-[15px] md:px-4"
+              placeholder={"digite seu email"}
+              value={email || ""}
+              onChange={(e) => {
+                e.preventDefault();
+                setEmail(e.target.value.trim().toLowerCase());
+              }}
+              onFocus={() => setEmailError("")}
+            />
+
+            {emailError && (
+              <p className="text-red-700 text-[12px] md:text-[13px] pl-0.5 mt-2">
+                {emailError}
+              </p>
+            )}
           </div>
-        </form>
+
+          <motion.button
+            whileTap={{ scale: 0.99 }}
+            whileHover={{ scale: 1.01 }}
+            className={`bg-roxominerva text-zinc-200 py-2.5 mt-[2px] text-[14px] rounded-[7px] md:text-[16px] flex items-center justify-center hover:bg-buttonHover hover:text-zinc-100 w-full`}
+            onClick={() => handleForgetPass()}
+          >
+            Enviar
+          </motion.button>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/reset_password/ResetPassForm.tsx
+++ src/components/reset_password/ResetPassForm.tsx
@@ -1,110 +1,103 @@
 "use client";
 
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { motion } from "motion/react";
+import { validatePassword } from "@/utils/regex";
+import { Eye } from "flowbite-react-icons/outline";
+import { EyeSlash } from "flowbite-react-icons/solid";
+import { useRouter } from "nextjs-toploader/app";
 
 const ForgetPassForm = () => {
-  const [finalizado, setFinalizado] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [isShow, setIsShow] = useState(false);
+  const router = useRouter();
+  const id = usePathname().slice(20);
 
-  //PEgando params
-  const _id = usePathname();
-  const realId = _id.slice(20);
+  const passwordRGX = validatePassword.test(password);
 
-  //Função pra mostar senha
-  const showPassFunction = () => {
-    const senha: any = document.getElementById("input-password");
-    if (senha.type === "password") {
-      senha.setAttribute("type", "text");
-    } else {
-      senha.setAttribute("type", "password");
-    }
-  };
-
-  const handleFormEdit = (event: any, name: string) => {
-    setFormData({ ...formData, [name]: event.target.value });
-  };
-
-  const handleForm = async (e: any) => {
+  const handleResetPass = async () => {
     try {
-      e.preventDefault();
+      if (!passwordRGX)
+        throw new Error("8 dígitos, uma letra maiúscula e um número.");
+
       //Enviando post dos dados
       const response = await fetch(
         `${process.env.HOST}/api/user/reset_password/`,
         {
           method: "PUT",
-          body: JSON.stringify(formData),
+          body: JSON.stringify({ password, id }),
         }
       );
-
-      setFinalizado(true);
     } catch (error: any) {
-      console.log(error.message);
+      setPasswordError(error.message);
     }
   };
-  //
-  //
-  const [formData, setFormData] = useState({
-    newPass: "",
-    _id: realId,
-  });
+
   return (
-    <>
-      {!finalizado ? (
-        <div className="px-8 md:self-center rounded-lg md:px-6 md:py-4 my-2 md:w-[400px] md:border-zinc-800 md:border-2 height_pattern w-full">
-          <form method="POST" className="form_reset_password">
-            <h1 className="h1_form">Redefinir Senha</h1>
-            <div className="div_form">
-              <div className="div_input_password">
-                <input
-                  type="password"
-                  name="password"
-                  id="input-password"
-                  placeholder="Nova senha"
-                  className="input_password"
-                  value={formData.newPass}
-                  required
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setFormData({
-                      ...formData,
-                      newPass: e.target.value,
-                    });
+    <section className="flex flex-col min-h-[76vh] font-inter text-black xl:min-h-[69.4vh] bg-background01 items-center">
+      <div className="bg-background02 p-8 rounded-md shadow-md mx-8 my-[32px] md:w-[400px]">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-[20px] md:text-[20px] lg:text-[22px] xl:text-[22px] text-[#353535]">
+            Redefinir Senha
+          </h1>
+
+          <p className="text-[14px] md:text-[15px] bg-background01 p-3 rounded-md">
+            Escolha uma nova senha para substituir a antiga.
+          </p>
+
+          <div>
+            <div className="text-[16px]">senha</div>
+            <div className="flex relative">
+              <input
+                type={isShow ? "text" : "password"}
+                id="input-password"
+                name="password"
+                value={password || ""}
+                className="flex items-center p-2.5 border-0 bg-background01 rounded-[7px] text-[14px] w-full mt-2 md:text-[15px] md:px-4"
+                placeholder={"digite sua senha"}
+                onChange={(e) => setPassword(e.target.value.trim())}
+                onFocus={() => setPasswordError("")}
+              />
+              {isShow ? (
+                <EyeSlash
+                  className="absolute z-[1] bottom-2.5 right-2.5 text-[#404040] md:right-4 cursor-pointer"
+                  onClick={() => {
+                    setIsShow(!isShow);
                   }}
                 />
-                <button
-                  type="button"
-                  className="btn_show_password"
-                  onClick={showPassFunction}
-                >
-                  show
-                </button>
-              </div>
+              ) : (
+                <Eye
+                  className="absolute z-[1] bottom-2.5 right-2.5 text-[#404040] md:right-4 cursor-pointer"
+                  onClick={() => {
+                    setIsShow(!isShow);
+                  }}
+                />
+              )}
             </div>
-            <button
-              type="submit"
-              className="btn_submit_form"
-              onClick={async (e) => {
-                await handleForm(e);
-                window.location.href = "/login";
-              }}
-            >
-              Redefinir
-            </button>
-          </form>
-        </div>
-      ) : (
-        <div className="flex justify-center items-center pt-20">
-          {" "}
-          <Link
-            href="/login"
-            className=" bg-roxominerva rounded-lg p-2 text-2xl"
+
+            {passwordError && (
+              <p className="text-red-700 text-[12px] md:text-[13px] text-center mt-3">
+                {passwordError}
+              </p>
+            )}
+          </div>
+
+          <motion.button
+            whileTap={{ scale: 0.99 }}
+            whileHover={{ scale: 1.01 }}
+            className={`bg-roxominerva text-zinc-200 py-2.5 mt-[2px] text-[14px] rounded-[7px] md:text-[16px] flex items-center justify-center hover:bg-buttonHover hover:text-zinc-100 w-full`}
+            onClick={() => {
+              handleResetPass();
+              router.push("/login");
+            }}
           >
-            Fazer Login
-          </Link>
+            Enviar
+          </motion.button>
         </div>
-      )}
-    </>
+      </div>
+    </section>
   );
 };
 
