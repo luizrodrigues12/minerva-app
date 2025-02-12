722d7b31f3a4d48762ab03c6efa32d687ee5471a
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2025-02-12 10:42:41 -0300

    fix: corrigindo bug de não aparecer meses e alunos no planejamento.

--- src/components/layout/SelectMonth.tsx
+++ src/components/layout/SelectMonth.tsx
@@ -2,6 +2,7 @@ import { AngleDown, AngleUp } from "flowbite-react-icons/outline";
 import { Dispatch, SetStateAction, useState } from "react";
 import InputComp from "./InputComp";
 import { allMonths } from "@/utils/months";
+import Loading from "./Loading";
 
 type SelectProps = {
   setMonthName: Dispatch<SetStateAction<string>>;
@@ -27,58 +28,64 @@ const SelectMonth = ({
   );
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="text-[16px] md:text-[18px]">Mês</div>
-      <div className="rounded-md relative">
-        <div
-          className="rounded-md bg-background03 flex justify-between items-center cursor-pointer"
-          onClick={() => {
-            setIsOpen(!isOpen);
-            setError("");
-          }}
-        >
-          <InputComp
-            className="placeholder:text-textColor cursor-pointer py-3 text-[14px] md:text-[16px]"
-            placeholder={monthName}
-            value={busca || ""}
-            onChange={(e) => setBusca(e.target.value)}
-          />
-          {isOpen ? (
-            <div className="px-3">
-              <AngleUp className="text-corIcones size-[22px]" />
+    <div>
+      {filteredMonths.length ? (
+        <div className="flex flex-col gap-2">
+          <div className="text-[16px] md:text-[18px]">Mês</div>
+          <div className="rounded-md relative">
+            <div
+              className="rounded-md bg-background03 flex justify-between items-center cursor-pointer"
+              onClick={() => {
+                setIsOpen(!isOpen);
+                setError("");
+              }}
+            >
+              <InputComp
+                className="placeholder:text-textColor cursor-pointer py-3 text-[14px] md:text-[16px]"
+                placeholder={monthName}
+                value={busca || ""}
+                onChange={(e) => setBusca(e.target.value)}
+              />
+              {isOpen ? (
+                <div className="px-3">
+                  <AngleUp className="text-corIcones size-[22px]" />
+                </div>
+              ) : (
+                <div className="px-3">
+                  <AngleDown className="text-corIcones size-[22px]" />
+                </div>
+              )}
             </div>
-          ) : (
-            <div className="px-3">
-              <AngleDown className="text-corIcones size-[22px]" />
-            </div>
-          )}
-        </div>
-        {isOpen && (
-          <div className="flex flex-col bg-background03 mt-1 rounded-md absolute z-[2] w-full max-h-[153px] md:max-h-[165px] overflow-y-scroll overflow-hidden scroll-style border-[1px] border-borderColor">
-            {filteredMonths.map((month, i) => (
-              <div
-                key={i}
-                className={`flex items-center py-2 hover:bg-background03Hover cursor-pointer text-[14px] md:text-[16px] ${
-                  i === filteredMonths.length! - 1
-                    ? "border-0 pb-2.5"
-                    : "border-b-[1px] border-borderColor"
-                }`}
-                onClick={() => {
-                  setMonthName(month.name);
-                  setMonthDays(month.dias);
-                  setMonthNumber(i);
-                  setBusca("");
-                  setIsOpen(false);
-                }}
-              >
-                <option key={i} className="px-3 text-[14px] md:text-[16px]">
-                  {month.name}
-                </option>
+            {isOpen && (
+              <div className="flex flex-col bg-background03 mt-1 rounded-md absolute z-[2] w-full max-h-[153px] md:max-h-[165px] overflow-y-scroll overflow-hidden scroll-style border-[1px] border-borderColor">
+                {filteredMonths.map((month, i) => (
+                  <div
+                    key={i}
+                    className={`flex items-center py-2 hover:bg-background03Hover cursor-pointer text-[14px] md:text-[16px] ${
+                      i === filteredMonths.length! - 1
+                        ? "border-0 pb-2.5"
+                        : "border-b-[1px] border-borderColor"
+                    }`}
+                    onClick={() => {
+                      setMonthName(month.name);
+                      setMonthDays(month.dias);
+                      setMonthNumber(i);
+                      setBusca("");
+                      setIsOpen(false);
+                    }}
+                  >
+                    <option key={i} className="px-3 text-[14px] md:text-[16px]">
+                      {month.name}
+                    </option>
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
--- src/components/layout/SelectStudent.tsx
+++ src/components/layout/SelectStudent.tsx
@@ -2,6 +2,7 @@ import { AngleDown, AngleUp } from "flowbite-react-icons/outline";
 import { Dispatch, SetStateAction, useState } from "react";
 import InputComp from "./InputComp";
 import { useUserContext } from "@/contexts/userData";
+import Loading from "./Loading";
 
 type SelectProps = {
   setOption: Dispatch<SetStateAction<string>>;
@@ -20,7 +21,6 @@ const SelectStudent = ({
 }: SelectProps) => {
   const [busca, setBusca] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-
   const { user } = useUserContext();
 
   const filteredStudents = user.alunos
@@ -31,56 +31,62 @@ const SelectStudent = ({
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-[16px] md:text-[18px]">Aluno</div>
-      <div className="rounded-md relative">
-        <div
-          className="rounded-md bg-background03 flex justify-between items-center cursor-pointer"
-          onClick={() => {
-            setIsOpen(!isOpen);
-            setError!("");
-          }}
-        >
-          <InputComp
-            className="placeholder:text-textColor cursor-pointer py-3 text-[14px] md:text-[16px]"
-            placeholder={option}
-            value={busca || ""}
-            onChange={(e) => setBusca(e.target.value)}
-          />
-          {isOpen ? (
-            <div className="px-3">
-              <AngleUp className="text-corIcones size-[22px]" />
+      {user ? (
+        <div className="flex flex-col gap-2">
+          <div className="text-[16px] md:text-[18px]">Aluno</div>
+          <div className="rounded-md relative">
+            <div
+              className="rounded-md bg-background03 flex justify-between items-center cursor-pointer"
+              onClick={() => {
+                setIsOpen(!isOpen);
+                setError!("");
+              }}
+            >
+              <InputComp
+                className="placeholder:text-textColor cursor-pointer py-3 text-[14px] md:text-[16px]"
+                placeholder={option}
+                value={busca || ""}
+                onChange={(e) => setBusca(e.target.value)}
+              />
+              {isOpen ? (
+                <div className="px-3">
+                  <AngleUp className="text-corIcones size-[22px]" />
+                </div>
+              ) : (
+                <div className="px-3">
+                  <AngleDown className="text-corIcones size-[22px]" />
+                </div>
+              )}
             </div>
-          ) : (
-            <div className="px-3">
-              <AngleDown className="text-corIcones size-[22px]" />
-            </div>
-          )}
-        </div>
-        {isOpen && (
-          <div className="flex flex-col bg-background03 mt-1 rounded-md absolute z-[2] w-full max-h-[126px] md:max-h-[134px] overflow-y-scroll overflow-hidden scroll-style border-[1px] border-borderColor">
-            {filteredStudents?.map((aluno, i) => (
-              <div
-                key={i}
-                className={`flex items-center py-2 hover:bg-background03Hover cursor-pointer text-[14px] md:text-[16px] ${
-                  i === filteredStudents.length! - 1
-                    ? "border-0 pb-2.5"
-                    : "border-b-[1px] border-borderColor"
-                }`}
-                onClick={() => {
-                  setOption(aluno.nome!);
-                  setValue(aluno.idAluno!);
-                  setBusca("");
-                  setIsOpen(false);
-                }}
-              >
-                <option key={i} className="px-3 text-[14px] md:text-[16px]">
-                  {aluno.nome}
-                </option>
+            {isOpen && (
+              <div className="flex flex-col bg-background03 mt-1 rounded-md absolute z-[2] w-full max-h-[126px] md:max-h-[134px] overflow-y-scroll overflow-hidden scroll-style border-[1px] border-borderColor">
+                {filteredStudents?.map((aluno, i) => (
+                  <div
+                    key={i}
+                    className={`flex items-center py-2 hover:bg-background03Hover cursor-pointer text-[14px] md:text-[16px] ${
+                      i === filteredStudents.length! - 1
+                        ? "border-0 pb-2.5"
+                        : "border-b-[1px] border-borderColor"
+                    }`}
+                    onClick={() => {
+                      setOption(aluno.nome!);
+                      setValue(aluno.idAluno!);
+                      setBusca("");
+                      setIsOpen(false);
+                    }}
+                  >
+                    <option key={i} className="px-3 text-[14px] md:text-[16px]">
+                      {aluno.nome}
+                    </option>
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
